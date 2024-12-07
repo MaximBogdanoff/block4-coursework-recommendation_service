01b01a11728d0da2bb7426df8a12fea1f34cc33f
Максим <bogdanoff.96@yandex.ru>
2024-12-08 04:52:02 +1100

    Initial commit

---
+++ src/main/java/com/skypro/block4_coursework_recommendation_service/Block4CourseworkRecommendationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.block4_coursework_recommendation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Block4CourseworkRecommendationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Block4CourseworkRecommendationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skypro/block4_coursework_recommendation_service/Block4CourseworkRecommendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.block4_coursework_recommendation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Block4CourseworkRecommendationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
